d7cf1e49252e27b9073ec6ec56a6991a5e18cfc3
danigb <danigb@gmail.com>
2017-03-08 17:02:27 +0100

    Assembler in js

---
+++ assembler-js/code.js
@@ -0,0 +1,9 @@
+
+/**
+ * Translates Hack assembly language mnemonics into binary codes.
+ */
+module.exports = { dest, comp, jump }
+
+function dest () {}
+function comp () {}
+function jump () {}
---
+++ assembler-js/parser.js
@@ -0,0 +1,25 @@
+
+/**
+ * The main function of the parser is to break each assembly command into its
+ * underlying components (fields and symbols)
+ *
+ * Encapsulates access to the input code. Reads an assembly language command,
+ * parses it, and provides convenient access to the command’s components (fields
+ * and symbols). In addition, removes all white space and comments.
+ */
+class Parser {
+  constructor (input) {
+
+  }
+
+  hasMoreCommands () {
+
+  }
+
+  advance () {}
+  commandType () {}
+  symbol () {}
+  dest () {}
+  comp () {}
+  jump () {}
+}
